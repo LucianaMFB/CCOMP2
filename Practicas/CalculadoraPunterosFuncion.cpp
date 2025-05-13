9817c92f2e084076ee13b74573c03defadb6ea0f
Luciana Milagros Farfan Begazo <lucianafarfanbegazo@gmail.com>
2025-05-12 20:45:26 -0500

    Update CalculadoraPunterosFuncion.cpp

--- Practicas/CalculadoraPunterosFuncion.cpp
+++ Practicas/CalculadoraPunterosFuncion.cpp
@@ -23,28 +23,27 @@ int divi(int a, int b) {
 
 void calculadora(int m, int n, int (*pfx)(int, int)) {
 	cout << endl;
-   cout << (*pfx)(m, n) << endl;
+   	cout << (*pfx)(m, n) << endl;
+	return;
 }
 
 void signo(int *a) {
-   cout << endl;
-   cout << "  sum(0) - sub(1) - mult(2) - div(3) " << endl;
-   cout << "  Ingrese el comando: ";
-   cin >> *a;
-   
-   return;
+	cout << endl;
+   	cout << "  sum(0) - sub(1) - mult(2) - div(3) " << endl;
+   	cout << "  Ingrese el comando: ";
+   	cin >> *a;
+   	return;
 }
 
 int main() {
 	int (*pf[])(int, int) = {sum, sub, mult, divi};
-
 	int m = 14;
 	int n = 17;
-   int sig = -1;
-   signo(&sig);
+	int sig = -1;
+	signo(&sig);
    
-   if ( sig < 4 && sig > -1) { calculadora(m, n, pf[sig]); }
+	if ( sig < 4 && sig > -1) { calculadora(m, n, pf[sig]); }
 	else { cout << "  Invalido" << endl; }
    
-   return 0;
+   	return 0;
 }
